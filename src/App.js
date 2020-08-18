edb109692a56e7bb3cdec743925f8cc576c3cbd6
Aylen HOZ <ahoz@candoit.com.ar>
2020-08-18 15:28:52 -0300

    Trabajando con listas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react';
-import ConditionalSection from './sections/conditional';
+import React, { Component } from 'react'
+import ConditionalSection from './sections/conditional'
+import pokemons from './data/pokemons.json'
+
+class PokemonItem extends Component {
+  render() {
+    const {pokemon} = this.props;
+    return (
+      <li>
+        <p><strong>Nombre: </strong>{pokemon.nombre}</p>
+        <p><strong>Tipo: </strong>{pokemon.tipo}</p>
+      </li>
+    );
+  };
+}
 
 class App extends Component {
-render() {
-  return (
-    <div className="App">
-    <ConditionalSection />
-    </div>
-  )
-}
+  render () {
+    const arr = [1, 1, 3, 4];
+    return (
+      <div className="App">
+        <ConditionalSection />
+        <h1>Trabajando con listas:</h1>
+        {arr.map((numero, index) => {
+            return <p key={index}>Soy el numero {numero}</p>
+          })
+        }
+        <h1>Trabajando con lista a partir de JSON:</h1>
+        <ul>
+          {pokemons.map(pokemon => {
+            return <PokemonItem key={pokemon.id} pokemon={pokemon} />
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
